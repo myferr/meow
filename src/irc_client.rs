ca91a9b86a91e6d9aad032d6a6d5e5d7beece47d
myferr <contactme.myfer@protonmail.com>
2025-06-29 10:59:06 -0500

    refactor(irc_client): Improve IRC message handling and error reporting

--- src/irc_client.rs
+++ src/irc_client.rs
@@ -42,83 +42,86 @@ pub async fn run_irc(irc_tx: Sender<String>, mut input_rx: Receiver<InputCommand
                                     let tx_clone = irc_tx.clone();
 
                                     tokio::spawn(async move {
-                                        let mut locked = client_clone.lock().await;
-                                        let mut stream = match locked.stream() {
-                                            Ok(s) => s,
-                                            Err(e) => {
-                                                let _ = tx_clone.send(format!("Error initializing IRC stream: {}", e)).await;
-                                                return;
+                                        // Lock only to get stream, then drop lock
+                                        let stream = {
+                                            let mut locked = client_clone.lock().await;
+                                            match locked.stream() {
+                                                Ok(s) => s,
+                                                Err(e) => {
+                                                    let _ = tx_clone.send(format!("Error initializing IRC stream: {}", e)).await;
+                                                    return;
+                                                }
                                             }
                                         };
 
-                                        while let Some(message) = stream.next().await.transpose().unwrap_or(None) {
-                                            match &message.command {
-                                                Command::PRIVMSG(target, msg) => {
-                                                    if msg.starts_with('\x01') && msg.ends_with('\x01') {
-                                                        let ctcp = &msg[1..msg.len() - 1];
-                                                        let sender = message.source_nickname().unwrap_or("unknown");
-                                                        let _ = tx_clone.send(format!("(CTCP) {}: {}", sender, ctcp)).await;
-                                                    } else if let Some(sender) = message.source_nickname() {
-                                                        let formatted = if target.starts_with('#') {
-                                                            format!("<{}> {}", sender, msg)
-                                                        } else {
-                                                            format!("<{}->You> {}", sender, msg)
-                                                        };
-                                                        let _ = tx_clone.send(formatted).await;
+                                        let mut stream = stream;
+
+                                        while let Some(message_result) = stream.next().await {
+                                            match message_result {
+                                                Ok(message) => {
+                                                    match &message.command {
+                                                        Command::PING(server, _) => {
+                                                            let _ = tx_clone.send(format!("*** Ping: {}", server)).await;
+                                                        }
+                                                        Command::JOIN(channel, _, _) => {
+                                                            let prefix = message.prefix.as_ref().map(|p| p.to_string()).unwrap_or_default();
+                                                            let _ = tx_clone.send(format!("*** {} joined {}", prefix, channel)).await;
+                                                        }
+                                                        Command::PRIVMSG(target, msg) => {
+                                                            if msg.starts_with('\x01') && msg.ends_with('\x01') {
+                                                                let ctcp = &msg[1..msg.len() - 1];
+                                                                let sender = message.source_nickname().unwrap_or("unknown");
+                                                                let _ = tx_clone.send(format!("(CTCP) {}: {}", sender, ctcp)).await;
+                                                            } else if let Some(sender) = message.source_nickname() {
+                                                                let formatted = if target.starts_with('#') {
+                                                                    format!("<{}> {}", sender, msg)
+                                                                } else {
+                                                                    format!("<{}->You> {}", sender, msg)
+                                                                };
+                                                                let _ = tx_clone.send(formatted).await;
+                                                            }
+                                                        }
+                                                        Command::NOTICE(target, msg) => {
+                                                            let _ = tx_clone.send(format!("(notice to {}): {}", target, msg)).await;
+                                                        }
+                                                        Command::Raw(cmd, params) if cmd == "MODE" => {
+                                                            let params_str = params.join(" ");
+                                                            let _ = tx_clone.send(format!("*** Mode: {}", params_str)).await;
+                                                        }
+                                                        Command::PART(channel, _) => {
+                                                            let _ = tx_clone.send(format!("*** Left channel {}", channel)).await;
+                                                        }
+                                                        Command::QUIT(reason) => {
+                                                            let reason_str = reason.as_ref().map(|r| r.to_string()).unwrap_or("Quit".into());
+                                                            if let Some(sender) = message.source_nickname() {
+                                                                let _ = tx_clone.send(format!("*** {} quit: {}", sender, reason_str)).await;
+                                                            }
+                                                        }
+                                                        Command::PONG(server, _) => {
+                                                            let _ = tx_clone.send(format!("*** Pong: {}", server)).await;
+                                                        }
+                                                        Command::Response(_code, params) => {
+                                                            let code = params.get(0).cloned().unwrap_or_default();
+                                                            let msg = params.iter().skip(1).cloned().collect::<Vec<_>>().join(" ");
+                                                            let display = match code.as_str() {
+                                                                "001" => format!("*** Welcome: {}", msg),
+                                                                "375" | "372" | "376" => format!("*** MOTD: {}", msg),
+                                                                _ => format!("*** {}: {}", code, msg),
+                                                            };
+                                                            let _ = tx_clone.send(display).await;
+                                                        }
+                                                        _ => {
+                                                            let _ = tx_clone.send(format!("*** Unhandled: {:?}", message.command)).await;
+                                                        }
                                                     }
                                                 }
-
-                                                Command::NOTICE(target, msg) => {
-                                                    let _ = tx_clone.send(format!("(notice to {}): {}", target, msg)).await;
-                                                }
-
-                                                Command::Raw(cmd, params) => {
-                                                    if cmd == "MODE" {
-                                                        let params_str = params.join(" ");
-                                                        let _ = tx_clone.send(format!("*** Mode: {}", params_str)).await;
-                                                    }
-                                                }
-
-                                                Command::JOIN(channel, _, _) => {
-                                                    let prefix = message.prefix.as_ref().map(|p| p.to_string()).unwrap_or_default();
-                                                    let _ = tx_clone.send(format!("*** {} joined {}", prefix, channel)).await;
-                                                }
-
-                                                Command::PART(channel, _) => {
-                                                    let _ = tx_clone.send(format!("*** Left channel {}", channel)).await;
-                                                }
-
-                                                Command::QUIT(reason) => {
-                                                    let reason_str = reason.as_ref().map(|r| r.to_string()).unwrap_or("Quit".into());
-                                                    if let Some(sender) = message.source_nickname() {
-                                                        let _ = tx_clone.send(format!("*** {} quit: {}", sender, reason_str)).await;
-                                                    }
-                                                }
-
-                                                Command::PING(server, _) => {
-                                                    let _ = tx_clone.send(format!("*** Ping: {}", server)).await;
-                                                }
-
-                                                Command::PONG(server, _) => {
-                                                    let _ = tx_clone.send(format!("*** Pong: {}", server)).await;
-                                                }
-
-                                                Command::Response(_code, params) => {
-                                                    let code = params.get(0).cloned().unwrap_or_default();
-                                                    let msg = params.iter().skip(1).cloned().collect::<Vec<_>>().join(" ");
-                                                    let display = match code.as_str() {
-                                                        "001" => format!("*** Welcome: {}", msg),
-                                                        "375" | "372" | "376" => format!("*** MOTD: {}", msg),
-                                                        _ => format!("*** {}: {}", code, msg),
-                                                    };
-                                                    let _ = tx_clone.send(display).await;
-                                                }
-
-                                                _ => {
-                                                    let _ = tx_clone.send(format!("*** Unhandled: {:?}", message.command)).await;
+                                                Err(e) => {
+                                                    let _ = tx_clone.send(format!("Error receiving message: {}", e)).await;
+                                                    break; // Exit on stream error, could reconnect if desired
                                                 }
                                             }
                                         }
+                                        let _ = tx_clone.send("*** IRC stream closed.".into()).await;
                                     });
 
                                     irc_tx.send(format!(
@@ -128,6 +131,7 @@ pub async fn run_irc(irc_tx: Sender<String>, mut input_rx: Receiver<InputCommand
                                         nick,
                                         if tls { "with" } else { "without" }
                                     )).await?;
+
                                     client_opt = Some(client);
                                 }
 
@@ -152,6 +156,8 @@ pub async fn run_irc(irc_tx: Sender<String>, mut input_rx: Receiver<InputCommand
                                         let _ = tx_clone.send(format!("<You->{}> {}", target_clone, message_clone)).await;
                                     }
                                 });
+                            } else {
+                                irc_tx.send("Not connected. Use /connect first.".into()).await?;
                             }
                         }
 
@@ -171,6 +177,8 @@ pub async fn run_irc(irc_tx: Sender<String>, mut input_rx: Receiver<InputCommand
                                 });
 
                                 current_channel = Some(channel);
+                            } else {
+                                irc_tx.send("Not connected. Use /connect first.".into()).await?;
                             }
                         }
 
@@ -192,6 +200,8 @@ pub async fn run_irc(irc_tx: Sender<String>, mut input_rx: Receiver<InputCommand
                                 if current_channel.as_ref() == Some(&channel) {
                                     current_channel = None;
                                 }
+                            } else {
+                                irc_tx.send("Not connected. Use /connect first.".into()).await?;
                             }
                         }
 
@@ -225,7 +235,7 @@ pub async fn run_irc(irc_tx: Sender<String>, mut input_rx: Receiver<InputCommand
                             }
                         }
                     },
-                    None => break,
+                    None => break, // input channel closed; exit loop
                 }
             }
         }
